2de6d63fe20340172a978b95110cdb467e4635ce
raj <rajmathur8409@gmail.com>
2025-12-08 22:06:26 +0530

    Changed db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,21 @@
-const {Sequelize } = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        port: process.env.DB_PORT,
-        logging: false,
-        timezone: '+00:00',
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: false,      // Railway allows non-SSL
+      },
+    },
+  }
 );
+
 module.exports = sequelize;
